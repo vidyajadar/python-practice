8d58fdca5c642a7ea939170733ff494bb89bc6e6
Vidya Jadar <vidyabjadar1998@gmail.com>
2026-06-17 08:16:40 +0530

    Found occurance of 2

--- List_Sum.py
+++ List_Sum.py
@@ -1,3 +1,9 @@
-nums = [10,25,8,40,15]
-Total = sum(nums)
-print(Total)
+nums = [1,2,3,2,4,2]
+count = 0
+for i in nums:
+    if i==2:
+        count+=1
+print(count)
+
+Sum = sum(nums)
+print("Sum of list=",Sum)
