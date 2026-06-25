2d106b5d48f3f237f9f068b64f3791808dcdbfd1
Vidya Jadar <vidyabjadar1998@gmail.com>
2026-06-25 11:57:45 +0530

    Learning objects in OOPS

--- OOPS.py
+++ OOPS.py
@@ -1,8 +1,11 @@
 class Student:
-    def __init__(self, fullname):
+    def __init__(self, fullname, RollNo):
         self.name = fullname
         print("adding new student in database..")
+        self.rollno = RollNo
+        print("Enter roll number")
 
-s1 = Student("Karan")
+s1 = Student("Karan", 23)
 print(s1.name)
+print(s1.rollno)
     
