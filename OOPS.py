a2b9f80317c4006a77051d2f109edb534f1c50eb
Vidya Jadar <vidyabjadar1998@gmail.com>
2026-06-26 09:19:09 +0530

    Learning methods

--- OOPS.py
+++ OOPS.py
@@ -1,11 +1,21 @@
 class Student:
+    college_name = "ABC College"
+
     def __init__(self, fullname, RollNo):
         self.name = fullname
         print("adding new student in database..")
-        self.rollno = RollNo
+        self.RollNo = RollNo
         print("Enter roll number")
+
+    def welcome(self):
+        print("Welcome student", self.name)
+    def get_RollNo(self):
+        return self.RollNo
 
 s1 = Student("Karan", 23)
 print(s1.name)
-print(s1.rollno)
+print(s1.RollNo)
+s1.welcome()
+s1.get_RollNo()
+
     
