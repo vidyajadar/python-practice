e2981c1babfb6e0a08fb986e34177f506b137536
Vidya Jadar <vidyabjadar1998@gmail.com>
2026-06-26 10:17:07 +0530

    Practiced abstaction and enacapsulation

--- Abstraction.py
+++ Abstraction.py
@@ -1,13 +1,24 @@
-class Car:
-    def __init__(self):
-        self.acc=False
-        self.brk=False
-        self.clutch=False
-    
-    def start(self):
-        self.clutch=True
-        self.acc=True
-        print("car started..")
+class Account:
 
-car1 = Car()
-car1.start()
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account = acc
+
+    #debit method
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs", amount, "was debited")
+        print("total balance=", self.get_balance())
+    
+    
+    #Credit method
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs", amount, "is credited")
+        print("total balance=", self.get_balance())
+
+    def get_balance(self):
+        return self.balance
+
+acc1 = Account(1400000, 12345)
+acc1.credit(30)
