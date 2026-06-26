6ed65670255a83327b6279f9ab754889241db5c3
Vidya Jadar <vidyabjadar1998@gmail.com>
2026-06-26 11:23:59 +0530

    Learning multi_inheritance

---
+++ multi_inheritance.py
@@ -0,0 +1,19 @@
+class Car:
+    @staticmethod
+    def start():
+        print("Car Started..")
+
+    @staticmethod
+    def stop():
+        print("Car stopped")
+
+class ToyotaCar(Car):
+    def __init__(self, brand):
+        self.brand = brand
+
+class Fortuner(ToyotaCar):
+    def __init__(self, type):
+        self.type = type
+
+car1 = Fortuner("diesel")
+car1.start()
