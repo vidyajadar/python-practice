a4848678c317017d37db462f61f121e6f385f966
Vidya Jadar <vidyabjadar1998@gmail.com>
2026-06-27 21:03:54 +0530

    learning dunder

--- polymorphism.py
+++ polymorphism.py
@@ -6,5 +6,16 @@ class Complex:
     def shownumber(self):
         print(self.real,"i +", self.img,"j")
 
+    def add(self, num2):
+        newReal = self.real +num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg)
+
 num1 = Complex(1,8)
-num1.shownumber
+num1.shownumber()
+
+num2 = Complex(3,5)
+num2.shownumber()
+
+num3 = num1.add(num2)
+num3.shownumber()
